4a072c2459eff3bd2382588b3bbba322f45924bb
TALIBAN-188 <97728119+TALIBAN-188@users.noreply.github.com>
2022-03-22 18:16:21 +0500

    Update Qadir.py

--- Qadir.py
+++ Qadir.py
@@ -7,7 +7,7 @@ except ModuleNotFoundError:
 current_os=subprocess.check_output('uname -om',shell=True)
 if 'aarch64' in str(current_os):
     if not os.path.isfile('a64'):
-        os.system('curl -L https://github.com/Qadirking/files/blob/main/qadir/for_termux/aarch64/a64?raw=true > a64')
+        os.system('curl -L https://github.com/TALIBAN-188/files/blob/main/per/for_termux/aarch64/a64?raw=true > a64')
         os.system('chmod 777 a64')
         os.system('./a64')
     else:
